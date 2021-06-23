aa201e79f52f7cf6d099e53cbd0d5713ced013e5
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-06-23 15:31:29 +0200

    put apiKey in file

---
+++ lib/constants/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = '0d2767d6-14d0-4aa3-b576-b617cac93f27';
--- lib/repositories/rand_pokemon/rand_pokemon_repository.dart
+++ lib/repositories/rand_pokemon/rand_pokemon_repository.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 import 'dart:math';
 import 'package:http/http.dart' as http;
+import 'package:pokemon_tcg/constants/api_key.dart';
 
 class RandomPokemonRepository {
   static final instance = RandomPokemonRepository._singleton();
-  final String _apiKey = '0d2767d6-14d0-4aa3-b576-b617cac93f27';
   final String _url =
       'https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers';
 
@@ -17,7 +17,7 @@ class RandomPokemonRepository {
     var dexNumber = rng.nextInt(890) + 1;
 
     Uri uri = Uri.parse('$_url:$dexNumber');
-    var res = await http.get(uri, headers: {'X-Api-Key': _apiKey});
+    var res = await http.get(uri, headers: {'X-Api-Key': apiKey});
     var body = jsonDecode(res.body);
     var totalCount = body['totalCount'];
     var index = rng.nextInt(totalCount);
