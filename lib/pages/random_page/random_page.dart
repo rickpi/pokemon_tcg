9cb7b00c86d64ffc955558e976f4e2d777d56c62
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-06-23 16:32:02 +0200

    load one pokemon at start

--- lib/pages/random_page/random_page.dart
+++ lib/pages/random_page/random_page.dart
@@ -1,11 +1,20 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokemon_tcg/blocs/rand_pokemon/rand_pokemon_bloc.dart';
+import 'package:pokemon_tcg/blocs/rand_pokemon/rand_pokemon_events.dart';
 import 'package:pokemon_tcg/pages/random_page/widgets/get_rand_button.dart';
 import 'package:pokemon_tcg/pages/random_page/widgets/pokemon_card.dart';
-import 'package:pokemon_tcg/style/purple_button.dart';
 
-class RandomPage extends StatelessWidget {
+class RandomPage extends StatefulWidget {
   const RandomPage({Key? key}) : super(key: key);
 
+  @override
+  _RandomPageState createState() => _RandomPageState();
+}
+
+class _RandomPageState extends State<RandomPage>
+    with AfterLayoutMixin<RandomPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,5 +32,10 @@ class RandomPage extends StatelessWidget {
             ),
           ],
         ));
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    BlocProvider.of<RandomPokemonBloc>(context).add(RandomPokemonEventGetOne());
   }
 }
