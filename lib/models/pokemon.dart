6de7ad4b4dabb7e95c2ccb24bfa9aa4a45c42867
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-06-24 11:48:57 +0200

    add isPokemonInList method

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -77,6 +77,15 @@ class Pokemon {
               ]
             : [];
 
+  static bool isPokemonInList(List<Pokemon> list, Pokemon pokemon) {
+    try {
+      list.firstWhere((item) => item.id == pokemon.id);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
   @override
   String toString() =>
       'id: $id\n' +
